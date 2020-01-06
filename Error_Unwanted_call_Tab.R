fd48a5b2252759d8b2973731db6c863224561df2
NLMK (Niels Krarup) <nlmk@novonordisk.com>
2020-01-06 14:46:36 +0100

    added print statements to see sequence of execution

--- Error_Unwanted_call_Tab.R
+++ Error_Unwanted_call_Tab.R
@@ -4,11 +4,16 @@ library(shiny)
 #Helper plot function, only needed on plots in tabs: Special and Cool
 plot_fun_helper <- function(x, y, tab){
     req(tab)
+  print("calling plot_fun_helper")
     switch(tab,
            "Special" = {
+             print("Special case in plot_fun_helper")
+             
                plot(x,y, type = 'l', col = 'green')  
            },
            "Cool" = {
+             print("Cool case in plot_fun_helper")
+             
                plot(x,y, type = 'b', pch = y, col = y ) 
            },
            stop("Error. Only defined for Tab: Special and Cool"))
@@ -17,16 +22,20 @@ plot_fun_helper <- function(x, y, tab){
 
 #function that returns the x- and y vektors for plotting, depending on the current tab.
 xy_dens_gen <- function(tab){
+  print("call to xy_dens_gen")
     req(tab)
     if(tab == "Normal"){
+      print("normal case in xy_dens_gen")
         x <- 1:100
         y <- x^2
     }
     else if(tab == "Special"){
+      print("Special case in xy_dens_gen")
         x <- 1:100
         y <- sin(x)
     }
     else if(tab =="Cool"){
+      print("Cool case in xy_dens_gen")
         x <- 1:100
         y <- x %% 5
     }
@@ -52,7 +61,7 @@ ui <-
         ),
         
         # Cool Tab 
-        tabPanel('Cool',       
+        tabPanel('Cool',      
                          plotOutput("plot_Cool")
         )
         
@@ -74,19 +83,29 @@ server <- function(input, output) {
     
     #Render print inside renderPLot
     output$plot_Normal <- renderPlot({
-        
+      if(input$cur_tab != "Normal") return(NULL)
+      
+        print("render PLot normal")
         #NB does not invoke the plot_fun_helper
         plot(rvals$x, rvals$y)
     })
     
     output$plot_Special <- renderPlot({
+      if(input$cur_tab != "Special") return(NULL)
+      
+      print("render PLot Special")
+      
         plot_fun_helper(x = rvals$x, y = rvals$y, tab = input$cur_tab)
     })
     
     output$plot_Cool <- renderPlot({
+      if(input$cur_tab != "Cool") return(NULL)
+      print("render PLot Cool")
+      
         plot_fun_helper(x = rvals$x, y = rvals$y, tab = input$cur_tab)
     })
     
+    print("-------------------")
     
 }
 
